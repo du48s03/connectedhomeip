52f2430aeb8b84bfe2fbdee3194801834f7f1906
Li-an Ding <dingl@amazon.com>
2022-02-21 16:17:30 -0800

    Add example

---
+++ examples/mode-select/linux/src/main-common.cpp
@@ -0,0 +1,121 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <app-common/zap-generated/att-storage.h>
+#include <app-common/zap-generated/attribute-type.h>
+#include <app-common/zap-generated/attributes/Accessors.h>
+#include <app/CommandHandler.h>
+#include <app/clusters/identify-server/identify-server.h>
+#include <app/clusters/network-commissioning/network-commissioning.h>
+#include <app/server/Server.h>
+#include <app/util/af.h>
+#include <lib/support/CHIPMem.h>
+#include <new>
+#include <platform/Linux/NetworkCommissioningDriver.h>
+#include <platform/PlatformManager.h>
+#include <system/SystemPacketBuffer.h>
+#include <transport/SessionManager.h>
+#include <transport/raw/PeerAddress.h>
+
+#include <Options.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::DeviceLayer;
+// Network commissioning
+namespace {
+// This file is being used by platforms other than Linux, so we need this check to disable related features since we only
+// implemented them on linux.
+constexpr EndpointId kNetworkCommissioningEndpointMain      = 0;
+constexpr EndpointId kNetworkCommissioningEndpointSecondary = 0xFFFE;
+
+#if CHIP_DEVICE_LAYER_TARGET_LINUX
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+NetworkCommissioning::LinuxThreadDriver sLinuxThreadDriver;
+Clusters::NetworkCommissioning::Instance sThreadNetworkCommissioningInstance(kNetworkCommissioningEndpointMain,
+                                                                             &sLinuxThreadDriver);
+#endif
+#if CHIP_DEVICE_CONFIG_ENABLE_WPA
+NetworkCommissioning::LinuxWiFiDriver sLinuxWiFiDriver;
+Clusters::NetworkCommissioning::Instance sWiFiNetworkCommissioningInstance(kNetworkCommissioningEndpointSecondary,
+                                                                           &sLinuxWiFiDriver);
+#endif
+
+Clusters::NetworkCommissioning::NullNetworkDriver sNullNetworkDriver;
+Clusters::NetworkCommissioning::Instance sNullNetworkCommissioningInstance(kNetworkCommissioningEndpointMain, &sNullNetworkDriver);
+
+#endif // CHIP_DEVICE_LAYER_TARGET_LINUX
+} // namespace
+
+void ApplicationInit()
+{
+    (void) kNetworkCommissioningEndpointMain;
+    // Enable secondary endpoint only when we need it, this should be applied to all platforms.
+    emberAfEndpointEnableDisable(kNetworkCommissioningEndpointSecondary, false);
+
+#if CHIP_DEVICE_LAYER_TARGET_LINUX
+    const bool kThreadEnabled = {
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+        LinuxDeviceOptions::GetInstance().mThread
+#else
+        false
+#endif
+    };
+
+    const bool kWiFiEnabled = {
+#if CHIP_DEVICE_CONFIG_ENABLE_WPA
+        LinuxDeviceOptions::GetInstance().mWiFi
+#else
+        false
+#endif
+    };
+
+    if (kThreadEnabled && kWiFiEnabled)
+    {
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+        sThreadNetworkCommissioningInstance.Init();
+#endif
+#if CHIP_DEVICE_CONFIG_ENABLE_WPA
+        sWiFiNetworkCommissioningInstance.Init();
+#endif
+        // Only enable secondary endpoint for network commissioning cluster when both WiFi and Thread are enabled.
+        emberAfEndpointEnableDisable(kNetworkCommissioningEndpointSecondary, true);
+    }
+    else if (kThreadEnabled)
+    {
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+        sThreadNetworkCommissioningInstance.Init();
+#endif
+    }
+    else if (kWiFiEnabled)
+    {
+#if CHIP_DEVICE_CONFIG_ENABLE_WPA
+        // If we only enable WiFi on this device, "move" WiFi instance to main NetworkCommissioning cluster endpoint.
+        sWiFiNetworkCommissioningInstance.~Instance();
+        new (&sWiFiNetworkCommissioningInstance)
+            Clusters::NetworkCommissioning::Instance(kNetworkCommissioningEndpointMain, &sLinuxWiFiDriver);
+        sWiFiNetworkCommissioningInstance.Init();
+#endif
+    }
+    else
+    {
+        // Use NullNetworkCommissioningInstance to disable the network commissioning functions.
+        sNullNetworkCommissioningInstance.Init();
+    }
+#endif // CHIP_DEVICE_LAYER_TARGET_LINUX
+}
